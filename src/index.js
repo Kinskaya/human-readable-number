5f705acceaf51c745176b65155d2f9b1a965162c
Kinskaya <kinskaya.nastya@gmail.com>
2021-02-24 22:44:50 +0300

    feat: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    let numOnes = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let numTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let numString = num.toString();
+
+    if (num === 0) {
+        return 'zero';
+    }
+
+    if (num < 20) {
+        return numOnes[num];
+    }
+
+    if (numString.length === 2) {
+        return (numTens[numString[0]] + ' ' + numOnes[numString[1]]).trim();
+    }
+
+    if (numString.length === 3) {
+        if (numString[1] === '0' && numString[2] === '0')
+            return numOnes[numString[0]] + ' hundred';
+        else
+            return numOnes[numString[0]] + ' hundred ' + toReadable(+(numString[1] + numString[2]));
+    }
 }
